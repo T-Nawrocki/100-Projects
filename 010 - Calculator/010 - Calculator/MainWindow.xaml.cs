7ec67c04885079b2645f1358ea2b22770398645d
Tomasz Nawrocki <tnawrocki@abacusnext.com>
2019-11-05 16:19:18 +0000

    Started implementing data validation for typed input. Made CurrentSumDisplay property IsHitTestVisible false so that it can be clicked/typed through while still sitting on top of IODisplay. Need to work on regex to make sure it's validating input correctly

--- 010 - Calculator/010 - Calculator/MainWindow.xaml.cs
+++ 010 - Calculator/010 - Calculator/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
 
 namespace _010___Calculator
 {
@@ -20,6 +22,8 @@ namespace _010___Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static readonly Regex _inputRegex = new Regex("^[0-9]+$"); // regex contains valid input for IODisplay
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,37 +31,39 @@ namespace _010___Calculator
 
         private void Button_MouseEnter(object sender, MouseEventArgs e)
         {
-
+            // change colour to highlight current mouse focus
         }
 
         private void Button_MouseLeave(object sender, MouseEventArgs e)
         {
-
+            // return colour to default (depending on style) after mouse focus leaves
         }
 
         private void Button_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            // change colour or add border to highlight mouse down
         }
 
         private void Button_MouseUp(object sender, MouseButtonEventArgs e)
         {
-
+            // return colour and border to default (depending on style) after mouse up
         }
 
         private void IODisplay_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-
+            // restricts typed input to valid characters only
+            e.Handled = !_inputRegex.IsMatch(e.Text);
         }
 
         private void IODisplay_Pasting(object sender, DataObjectPastingEventArgs e)
         {
-
+            // restricts pasted input to valid characters only
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-
+            // master event handler for button clicks. 
+            // Might make sense to split this into separate handlers for each type of button (maybe one for numberbuttons, and one each for the others)
         }
     }
 }
